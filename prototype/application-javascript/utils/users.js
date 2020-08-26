a82b66b5eb8e9dc8f76cc59c72db39a232fa7eb3
Amel Dussier <amel.dussier@gmail.com>
2020-08-26 21:19:00 +0200

    Login page & user session

--- prototype/application-javascript/utils/users.js
+++ prototype/application-javascript/utils/users.js
@@ -3,6 +3,7 @@
 const adminUserId = 'admin';
 const studentRole = 'student';
 const teacherRole = 'teacher';
+const secretariatRole = 'secretariat';
 
 async function getAllIdentities(caClient, wallet, role) {
 	try {
@@ -19,8 +20,8 @@ async function getAllIdentities(caClient, wallet, role) {
 		const adminUser = await provider.getUserContext(adminIdentity, adminUserId);
 
 		const identityService = caClient.newIdentityService();
-		let serviceResponse = await identityService.getAll(adminUser);
-		return serviceResponse.result.identities.filter(i => i.attrs.some(a => a.name === 'role' && a.value === role));
+		let response = await identityService.getAll(adminUser);
+		return response.result.identities.filter(i => i.attrs.some(a => a.name === 'role' && a.value === role));
 	} catch (error) {
 		console.error(`Failed to list identities : ${error}`);
 	}
@@ -42,16 +43,61 @@ exports.listTeachers = async (caClient, wallet) => {
 	}
 };
 
-async function registerAndEnrollUser(caClient, wallet, orgMspId, userId, secret, firstname, lastname, role) {
-    try {
-		// Check to see if we've already enrolled the user
-		const userIdentity = await wallet.get(userId);
-		if (userIdentity) {
-			console.log(`An identity for the user ${userId} already exists in the wallet`);
-			return;
-		}
+exports.enrollUser = async (caClient, wallet, orgMspId, username, password) => {
 
-		// Must use an admin to register a new user
+	// Must use an admin
+	const adminIdentity = await wallet.get(adminUserId);
+	if (!adminIdentity) {
+		console.log('An identity for the admin user does not exist in the wallet');
+		console.log('Enroll the admin user before retrying');
+		return;
+	}
+
+	// enroll user
+	const enrollment = await caClient.enroll({
+		enrollmentID: username,
+		enrollmentSecret: password,
+		attr_reqs: [
+			{ name: 'firstname', optional: false },
+			{ name: 'lastname', optional: false },
+			{ name: 'role', optional: false }
+		]
+	});
+
+	// generate identity for wallet
+	const x509Identity = {
+		credentials: {
+			certificate: enrollment.certificate,
+			privateKey: enrollment.key.toBytes(),
+		},
+		mspId: orgMspId,
+		type: 'X.509',
+	};
+
+	// add identity to wallet
+	await wallet.put(username, x509Identity);
+
+	// build a user object for authenticating with the CA
+	const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+	const adminUser = await provider.getUserContext(adminIdentity, adminUserId);
+	const identityService = caClient.newIdentityService();
+
+	// get user
+	let response = await identityService.getOne(username, adminUser);
+
+	console.log(`Successfully enrolled user ${username} and imported it into the wallet`);
+
+	return {
+		username: username,
+		firstname: response.result.attrs.filter(a => a.name === 'firstname')[0].value,
+		lastname: response.result.attrs.filter(a => a.name === 'lastname')[0].value,
+		role: response.result.attrs.filter(a => a.name === 'role')[0].value
+	};
+};
+
+async function registerUser(caClient, wallet, username, password, firstname, lastname, role) {
+	try {
+		// Must use an admin
 		const adminIdentity = await wallet.get(adminUserId);
 		if (!adminIdentity) {
 			console.log('An identity for the admin user does not exist in the wallet');
@@ -62,70 +108,64 @@ async function registerAndEnrollUser(caClient, wallet, orgMspId, userId, secret,
 		// build a user object for authenticating with the CA
 		const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
 		const adminUser = await provider.getUserContext(adminIdentity, adminUserId);
+		const identityService = caClient.newIdentityService();
 
-		// Register the user, enroll the user, and import the new identity into the wallet.
-		// if affiliation is specified by client, the affiliation value must be configured in CA
+		// check if user exist
+		try {
+			let user = await identityService.getOne(username, adminUser);
+			if (user) {
+				console.error(`User already exist: ${username}`);
+				return;
+			}
+		}
+		catch (error) {
+
+		}
+
+		// register new user
 		await caClient.register({
-            enrollmentID: userId,
-            enrollmentSecret: secret,
-            role: 'client',
-            attrs: [
-                {
-                    name: 'firstname',
-                    value: firstname,
-                    ecert: true
-                },
-                {
-                    name: 'lastname',
-                    value: lastname,
-                    ecert: true
-                },
-                {
-                    name: 'role',
-                    value: role,
-                    ecert: true
-                }
-            ]
-        }, adminUser);
-		const enrollment = await caClient.enroll({
-			enrollmentID: userId,
-            enrollmentSecret: secret,
-            attr_reqs: [
-                { name: 'role', optional: false }
-            ]
-		});
-		const x509Identity = {
-			credentials: {
-				certificate: enrollment.certificate,
-				privateKey: enrollment.key.toBytes(),
-			},
-			mspId: orgMspId,
-			type: 'X.509',
-		};
-		await wallet.put(userId, x509Identity);
-        console.log(`Successfully registered and enrolled user ${userId} and imported it into the wallet`);
+			enrollmentID: username,
+			enrollmentSecret: password,
+			role: 'client',
+			maxEnrollments: -1,
+			attrs: [
+				{
+					name: 'firstname',
+					value: firstname,
+					ecert: true
+				},
+				{
+					name: 'lastname',
+					value: lastname,
+					ecert: true
+				},
+				{
+					name: 'role',
+					value: role,
+					ecert: true
+				}
+			]
+		}, adminUser);
+
+		console.log(`Successfully registered user ${username}`);
 	} catch (error) {
 		console.error(`Failed to register user : ${error}`);
 	}
 };
 
-exports.addStudent = async (caClient, wallet, orgMspId, userId, secret, firstname, lastname) => {
-	try {
-		await registerAndEnrollUser(caClient, wallet, orgMspId, userId, secret, firstname, lastname, studentRole);
-	} catch (error) {
-		console.error(`Failed to add student : ${error}`);	
-	}
+exports.addStudent = async (caClient, wallet, username, password, firstname, lastname) => {
+	await registerUser(caClient, wallet, username, password, firstname, lastname, studentRole);
 };
 
-exports.addTeacher = async (caClient, wallet, orgMspId, userId, secret, firstname, lastname) => {
-	try {
-		await registerAndEnrollUser(caClient, wallet, orgMspId, userId, secret, firstname, lastname, teacherRole);
-	} catch (error) {
-		console.error(`Failed to add teacher : ${error}`);	
-	}
+exports.addTeacher = async (caClient, wallet, username, password, firstname, lastname) => {
+	await registerUser(caClient, wallet, username, password, firstname, lastname, teacherRole);
 };
 
-exports.removeUser = async (caClient, wallet, userId) => {
+exports.addSecretariat = async (caClient, wallet, username, password, firstname, lastname) => {
+	await registerUser(caClient, wallet, username, password, firstname, lastname, secretariatRole);
+};
+
+exports.removeUser = async (caClient, wallet, username) => {
 	try {
 		// Must use an admin
 		const adminIdentity = await wallet.get(adminUserId);
@@ -135,19 +175,21 @@ exports.removeUser = async (caClient, wallet, userId) => {
 			return;
 		}
 
-		// Must use an admin
-		const userIdentity = await wallet.get(userId);
+		// delete identity from wallet
+		const userIdentity = await wallet.get(username);
 		if (userIdentity) {
 			console.log('Delete user from wallet');
-			await wallet.remove(userId);
+			await wallet.remove(username);
 		}
 
 		// build a user object for authenticating with the CA
 		const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
 		const adminUser = await provider.getUserContext(adminIdentity, adminUserId);
-
 		const identityService = caClient.newIdentityService();
-		await identityService.delete(userId, adminUser);
+
+		// delete user
+		await identityService.delete(username, adminUser);
+
 	} catch (error) {
 		console.error(`Failed to list identities : ${error}`);
 	}
